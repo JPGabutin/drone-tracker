14694dd2c681d67b41a532c5f20599add1398e77
jpgab <gabutin.jp@gmail.com>
2026-07-12 15:04:26 +0800

    fix: eagerly fetch loaded medications for complete loading response

--- src/main/java/com/jpg/drone_tracker/application/component/drone/repository/DroneRepository.java
+++ src/main/java/com/jpg/drone_tracker/application/component/drone/repository/DroneRepository.java
@@ -13,7 +13,7 @@ public interface DroneRepository extends JpaRepository<Drone, UUID> {
 
     Optional<Drone> findBySerialNumber(String serialNumber);
 
-    @EntityGraph(attributePaths = "loadedMedications")
+    @EntityGraph(attributePaths = {"loadedMedications", "loadedMedications.medication"})
     @Query("select d from Drone d where d.serialNumber = :serialNumber")
     Optional<Drone> findBySerialNumberWithLoadedMedications(@Param("serialNumber") String serialNumber);
 }
